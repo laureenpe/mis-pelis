5f5558296ca2061cdb7bacaedcb79e50601abe18
Antonia Cerda <antoniacerdag@gmail.com>
2017-08-10 10:47:25 -0400

    nav

--- dist/js/script.js
+++ dist/js/script.js
@@ -20254,5 +20254,14 @@ if (jQuery) {
 })(jQuery);
 
 $(document).ready(function() {
-	
+
 });
+
+	//funcion para que se despliegue el menu lateral
+	function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+	}
+	//funcion para cerrar el menu lateral
+	function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+	}
