3c68f6f4d011c4ca85a9c249b57b6c1b1e7b374e
“Laura <laureen.pelaez.isaza@gmail.com>
2017-08-10 11:54:34 -0400

    variables css updated, merge js

--- dist/js/script.js
+++ dist/js/script.js
@@ -20256,3 +20256,48 @@ if (jQuery) {
 $(document).ready(function() {
 	
 });
+
+
+
+$(document).ready(function () {
+    getFromLocalStorage(); //obtener local storage
+});
+//This function save to local storage
+function saveToLocalStorage() {
+    if (typeof (Storage) !== "undefined") {//soporte del navegador
+       
+        if (photo_url != '') {//si la foto es diferente de vacìo
+            localStorage.setItem('photo', photo_url);
+        }
+        if ($('#firstname').val() != '') {//si el nombre es diferente de vacío
+            localStorage.setItem('firstname', $('#firstname').val());
+        }
+        if ($('#username').val() != ''){//si el valor es diferente de vacìo
+            localStorage.setItem('username', $('#username').val());
+        }
+        if ($('#email').val() != ''){//si el valor es diferente de vacìo
+            localStorage.setItem('email', $('#email').val());
+        }
+        if ($('#country').val() != ''){//si el valor es diferente de vacìo
+            localStorage.setItem('country', $('#country').val());
+        }
+
+        alert('Datos actualizados');       
+        
+    } else {
+        //No hay soporte de navegador
+        console.log('Sorry there is not support for local storage.')
+    }
+}
+
+function getFromLocalStorage() {
+    console.log('getting info for: ' + localStorage.getItem('firstname'));
+    $('#photo').attr('src', localStorage.getItem('photo'));
+    $('#firstname').val(localStorage.getItem('firstname'));
+    $('#username').val(localStorage.getItem('username'));
+    $('#email').val(localStorage.getItem('email'));
+    $('#country').val(localStorage.getItem('country'));
+}
+
+//This function validate the login 
+//This function validate the create user
