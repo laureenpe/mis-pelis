993902123a9e5482c1b012e816de721b5fbe0f9c
“Laura <laureen.pelaez.isaza@gmail.com>
2017-08-10 15:15:52 -0400

    modified html registro and include validations

--- dist/js/script.js
+++ dist/js/script.js
@@ -20286,6 +20286,7 @@ $(document).ready(function() {
 
 
 $(document).ready(function () {
+    $("#btn-upload").click(handleFileSelect);//handleFileSelect, extract the files on input
     $('#sign-session').click(onLogin);//listener to button click
     getFromLocalStorage(); //obtener local storage
 });
@@ -20346,6 +20347,28 @@ function onLogin() {
     if (validateForm()) { //If validate form is True
         $("#sign-session").attr("href", "movies.html");
         saveToLocalStorage(); //guarda a local storage
+    }
+}
+//Function to save images in local storage
+function handleFileSelect() {
+    var files = $('#files')[0].files; 
+    // Gets the image selected on input and assign them to image. Get from https://www.html5rocks.com/en/tutorials/file/dndfiles/
+    for (var i = 0, file; file = files[i]; i++) {
+        // Si la imagen coincide, proseguir
+        if (!file.type.match('image.*')) {
+            continue;
+        }
+        var reader = new FileReader(); //object js to process the image
+        // when load image
+        reader.onload = (function (theFile) {
+            return function (e) {
+                // Render thumbnail.
+                $('#photo').attr('src', e.target.result);//assign the image to the function
+                photo_url = e.target.result;
+            };
+        })(file);
+        // lee la imagen como una URL
+        reader.readAsDataURL(file);
     }
 }
 //This function validate the create user
