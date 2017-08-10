4cdbfeb18c369c178e29157b3978e4739a327e8c
“Laura <laureen.pelaez.isaza@gmail.com>
2017-08-10 12:21:22 -0400

    on login and validate form updated

--- assets/js/validations.js
+++ assets/js/validations.js
@@ -1,5 +1,6 @@
 
 $(document).ready(function () {
+    $('#sign-session').click(onLogin);//listener to button click
     getFromLocalStorage(); //obtener local storage
 });
 //This function save to local storage
@@ -21,7 +22,6 @@ function saveToLocalStorage() {
         if ($('#country').val() != '') {//si el valor es diferente de vacìo
             localStorage.setItem('country', $('#country').val());
         }
-
         alert('Datos actualizados');
 
     } else {
@@ -48,6 +48,29 @@ function login() {
         $("#email").append($("#email").val("Ingrese un correo válido"));
     } else {
         $("#next").attr("href", "movies.html");
+    }
+}
+//This function validate the name and email and put a red border in case of error
+function validateForm() {
+    var valid = true;
+    if (!(/^([a-zñáéíóú]{2,13})+$/.test($("#firstname").val()))) {
+        $("#firstname").css("border", "1px solid red");
+        alert('El nombre debe ser válido');
+        valid = false;
+    }
+
+    if (!(/^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,4})$/.test($('#email').val()))) {
+        $("#email").css("border", "1px solid red");
+        alert('Error en el email');
+        valid = false;
+    }
+    return valid;
+}
+// If validations are true, then go to movies.html and save it into 
+function onLogin() {
+    if (validateForm()) { //If validate form is True
+        $("#sign-session").attr("href", "movies.html");
+        saveToLocalStorage(); //guarda a local storage
     }
 }
 //This function validate the create user
