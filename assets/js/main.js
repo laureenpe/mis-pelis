e526da5c78da2787771dc159d6a4a42962cb92d4
Antonia Cerda <antoniacerdag@gmail.com>
2017-08-11 12:27:54 -0400

    listo

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,13 +18,13 @@ $(document).ready(function() {
 			$(".mostrar-pelis").append("<div class='movie'><div class='row'><div class='col m8 s8'><div class='titulos'><a href='details.html' class='movie-title'>"+titulo+"</a><p class='movie-year'>"+anio+"</p><div class='lineavert'></div><p class='category'>"+categoria+"</p></div></div><div class='col m4 s4'><button class='add-favorite'>Add favorites</button></div></div><div class='row'><div class='col m8 s8'><div class='movie-details'><i class='fa fa-clock-o' aria-hidden='true'></i><p class='movie-minutes'>"+duracion+"</p><i class='fa fa-film' aria-hidden='true'></i><p class='movie-director'>"+director+"</p></div></div><div class='col m4 s4'><i class='fa fa-star' aria-hidden='true'></i></div></div></div>");
 
 			//para mostrar detalles de la pelicula
-			var img = ele.poster;
+			/*var img = ele.poster;
 			console.log(img);
 		     var nombrePelicula = ele.show_title;
 		     var summary = ele.summary;
 		     var categoria = ele.category;
 		     var director = ele.director;
-		     var actores = ele.show_cast;
+		     var actores = ele.show_cast;*/
 
 
 			//funcion para select de categorías
