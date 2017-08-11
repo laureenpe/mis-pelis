d05f77591e484e2d6cda08abc89eeb766c87146e
Antonia Cerda <antoniacerdag@gmail.com>
2017-08-11 10:39:03 -0400

    maqueta select

--- dist/js/script.js
+++ dist/js/script.js
@@ -20261,18 +20261,32 @@ $(document).ready(function() {
 	req.open('GET', URL + queryType + director, true);
 	req.addEventListener("load", function() {
 		var response = JSON.parse(req.responseText);
-		console.log(response);
+		//console.log(response);
 		response.forEach(function(ele){
-			console.log(ele);
+			//console.log(ele);
 			var titulo = ele.show_title;
 			var anio = ele.release_year;
 			var categoria = ele.category;
 			var duracion = ele.runtime;
 			var director = ele.director;
-			$(".mostrar-pelis").append("<div class='movie'><div class='row'><div class='col m8 s8'><div class='titulos'><a href='' class='movie-title'>"+titulo+"</a><p class='movie-year'>"+anio+"</p><div class='lineavert'></div><p class='category'>"+categoria+"</p></div></div><div class='col m4 s4'><button class='add-favorite'>Add favorites</button></div></div><div class='row'><div class='col m8 s8'><div class='movie-details'><i class='fa fa-clock-o' aria-hidden='true'></i><p class='movie-minutes'>"+duracion+"</p><i class='fa fa-film' aria-hidden='true'></i><p class='movie-director'>"+director+"</p></div></div><div class='col m4 s4'><i class='fa fa-star' aria-hidden='true'></i></div></div></div>")
+			$(".mostrar-pelis").append("<div class='movie'><div class='row'><div class='col m8 s8'><div class='titulos'><a href='' class='movie-title'>"+titulo+"</a><p class='movie-year'>"+anio+"</p><div class='lineavert'></div><p class='category'>"+categoria+"</p></div></div><div class='col m4 s4'><button class='add-favorite'>Add favorites</button></div></div><div class='row'><div class='col m8 s8'><div class='movie-details'><i class='fa fa-clock-o' aria-hidden='true'></i><p class='movie-minutes'>"+duracion+"</p><i class='fa fa-film' aria-hidden='true'></i><p class='movie-director'>"+director+"</p></div></div><div class='col m4 s4'><i class='fa fa-star' aria-hidden='true'></i></div></div></div>");
+
+			//funcion para select de categorías
+
+			var select = $('#select-category option:selected')[0].value;
+			console.log(select);
+			$(".filtrar").click(function(){
+				if(select == categoria){
+					$(".mostrar-pelis").append("<div class='movie'><div class='row'><div class='col m8 s8'><div class='titulos'><a href='' class='movie-title'>"+titulo+"</a><p class='movie-year'>"+anio+"</p><div class='lineavert'></div><p class='category'>"+categoria+"</p></div></div><div class='col m4 s4'><button class='add-favorite'>Add favorites</button></div></div><div class='row'><div class='col m8 s8'><div class='movie-details'><i class='fa fa-clock-o' aria-hidden='true'></i><p class='movie-minutes'>"+duracion+"</p><i class='fa fa-film' aria-hidden='true'></i><p class='movie-director'>"+director+"</p></div></div><div class='col m4 s4'><i class='fa fa-star' aria-hidden='true'></i></div></div></div>");
+				}
+				//console.log(select.category);
+				
+			});
+			
 		});
 	});
 	req.send(null);
+
 });
 
 	//funcion para que se despliegue el menu lateral
