26ac9d545e83c2c4e029e00edd0695c33a6d1a15
“Laura <laureen.pelaez.isaza@gmail.com>
2017-08-15 00:17:45 -0300

    updating styles, working comedies filter

--- dist/js/script.js
+++ dist/js/script.js
@@ -20262,14 +20262,14 @@ $(document).ready(function () {
 	req.open('GET', URL + queryType + director, true);
 	req.addEventListener("load", function () {
 		 response = JSON.parse(req.responseText);
-		//console.log(response);
+		console.log(response);
 		response.forEach(function (ele) {
 			var titulo = ele.show_title;
 			var anio = ele.release_year;
 			var categoria = ele.category;
 			var duracion = ele.runtime;
 			var director = ele.director;
-			$(".mostrar-pelis").append("<div class='movie'><div class='row'><div class='col m8 s8'><div class='titulos'><a href='details.html' class='movie-title'>" + titulo + "</a><p class='movie-year'>" + anio + "</p><div class='lineavert'></div><p class='category'>" + categoria + "</p></div></div><div class='col m4 s4'><button class='add-favorite'>Add favorites</button></div></div><div class='row'><div class='col m8 s8'><div class='movie-details'><i class='fa fa-clock-o' aria-hidden='true'></i><p class='movie-minutes'>" + duracion + "</p><i class='fa fa-film' aria-hidden='true'></i><p class='movie-director'>" + director + "</p></div></div><div class='col m4 s4'><i class='fa fa-star' aria-hidden='true'></i></div></div></div>");
+			$(".mostrar-pelis").append("<div class='movie'><div class='row'><div class='col m8 s8'><div class='titulos'><a href='details.html' class='movie-title'>" + titulo + "</a><p class='movie-year'>" + anio + "</p><div class='lineavert'></div><p class='category'>" + categoria + "</p></div></div><div class='col m4 s4'><button class='add-favorite'>Add favorites</button></div></div><div class='row'><div class='col m8 s8'><div class='movie-details'><i class='fa fa-clock-o' aria-hidden='true'></i><p class='movie-minutes'>" + duracion + "</p><i class='fa fa-film' aria-hidden='true'></i><p class='movie-director'>" + director + "</p></div></div><div class='col m4 s4'><i class='fa fa-star' aria-hidden='true'></i><i class='fa fa-star' aria-hidden='true'></i><i class='fa fa-star' aria-hidden='true'></i></div></div></div>");
 
 			//para mostrar detalles de la pelicula
 			/*var img = ele.poster;
@@ -20291,8 +20291,9 @@ $(document).ready(function () {
 		var select = $("#select-category").val();
 		$(".mostrar-pelis").html("");
 		var filtered = response.filter(object => object.category == select);
-		console.log(filtered);
+		//console.log(filtered);
 		filtered.forEach(function (ele) {
+			
 			var titulo = ele.show_title;
 			var anio = ele.release_year;
 			var categoria = ele.category;
