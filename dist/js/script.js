cc9c4393e3d960388b4a084233e5c7cbb07f7161
“Laura <laureen.pelaez.isaza@gmail.com>
2017-08-14 21:01:16 -0300

    working filter 100%

--- dist/js/script.js
+++ dist/js/script.js
@@ -20253,18 +20253,17 @@ if (jQuery) {
   };
 })(jQuery);
 
+var response;//global variable
 $(document).ready(function () {
-
 	var req = new XMLHttpRequest();
 	var URL = "https://netflixroulette.net/api/api.php?";
 	var queryType = "director=";
 	var director = "Quentin%20Tarantino";
 	req.open('GET', URL + queryType + director, true);
 	req.addEventListener("load", function () {
-		var response = JSON.parse(req.responseText);
+		 response = JSON.parse(req.responseText);
 		//console.log(response);
 		response.forEach(function (ele) {
-			console.log(ele);
 			var titulo = ele.show_title;
 			var anio = ele.release_year;
 			var categoria = ele.category;
@@ -20290,7 +20289,17 @@ $(document).ready(function () {
 	
 	$("#filtrar").click(function () {
 		var select = $("#select-category").val();
-		alert("you choose the category " + " "+ select);
+		$(".mostrar-pelis").html("");
+		var filtered = response.filter(object => object.category == select);
+		console.log(filtered);
+		filtered.forEach(function (ele) {
+			var titulo = ele.show_title;
+			var anio = ele.release_year;
+			var categoria = ele.category;
+			var duracion = ele.runtime;
+			var director = ele.director;
+			$(".mostrar-pelis").append("<div class='movie'><div class='row'><div class='col m8 s8'><div class='titulos'><a href='details.html' class='movie-title'>" + titulo + "</a><p class='movie-year'>" + anio + "</p><div class='lineavert'></div><p class='category'>" + categoria + "</p></div></div><div class='col m4 s4'><button class='add-favorite'>Add favorites</button></div></div><div class='row'><div class='col m8 s8'><div class='movie-details'><i class='fa fa-clock-o' aria-hidden='true'></i><p class='movie-minutes'>" + duracion + "</p><i class='fa fa-film' aria-hidden='true'></i><p class='movie-director'>" + director + "</p></div></div><div class='col m4 s4'><i class='fa fa-star' aria-hidden='true'></i></div></div></div>");
+		});
 	});
 });
 
